8f2e1f7d7f1a1af6b44c31ecb999a46180826aeb
Travis Swift <travis.arthur.swift@gmail.com>
2022-06-01 18:27:03 -0500

    updated code

--- server.js
+++ server.js
@@ -18,4 +18,3 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
-
