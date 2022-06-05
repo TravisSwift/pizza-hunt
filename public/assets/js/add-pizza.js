2052edeece0f2eacef8af575d3911c4c65377c18
Travis Swift <travis.arthur.swift@gmail.com>
2022-06-05 13:45:14 -0500

    code snapshot

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -68,8 +68,8 @@ const handlePizzaSubmit = event => {
     })
     .catch(err => {
       console.log(err);
+      saveRecord(formData);
     });
-};
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
 $addToppingBtn.addEventListener('click', handleAddTopping);
